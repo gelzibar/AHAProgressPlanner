4368f5d3eece30b8e7a59b7014c8614b48b2d762
gelzibar <anjo.hill@gmail.com>
2022-11-01 23:48:17 -0500

    Exploring vuejs functionality thru scaffolding changes

---
+++ aha-progress-planner/src/components/NavigationBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <nav>
+        <RouterLink to="/">Lobby</RouterLink>
+        <RouterLink to="/about">Progress</RouterLink>
+        <RouterLink to="/arsenal">Roster</RouterLink>
+        <RouterLink to="/arsenal">Recruitment</RouterLink>
+        <RouterLink to="/arsenal">Store</RouterLink>
+        <RouterLink to="/arsenal">Arsenal</RouterLink>
+        <RouterLink to="/arsenal">Library</RouterLink>
+
+      </nav>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ aha-progress-planner/src/views/ArsenalView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Arsenal from "../components/Arsenal.vue";
+</script>
+
+<template>
+    <Arsenal />
+</template>
+  
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
+  
